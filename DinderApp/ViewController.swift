4b67b3be24aae9a9b82273838295f8a029b7e0b7
fregidorsalazar <fregidorsalazar@gmail.com>
2024-09-19 09:52:14 +0200

    Initial Commit

---
+++ DinderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DinderApp
+//
+//  Created by Mañanas on 19/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
