09f6829bd8d75b0996f72f80a6f0716c96e7691f
fregidorsalazar <fregidorsalazar@gmail.com>
2024-09-20 10:26:02 +0200

    avanzando, creada interfaz de login

--- DinderApp/ViewController.swift
+++ DinderApp/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var mainView: UIView?
+    @IBOutlet weak var mainView: UIView!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var logInButton: UIButton!
+    
+    @IBOutlet weak var signInButton: UIButton!
     
     override func viewDidLoad() {
-        mainView?.layer.cornerRadius = 12
+        mainView!.layer.cornerRadius = 12
         mainView!.layer.masksToBounds = true
+        title = "Inicio de Sesión"
         
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ Pods/Target Support Files/Pods-DinderApp/Pods-DinderApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DinderApp : NSObject
+@end
+@implementation PodsDummy_Pods_DinderApp
+@end
